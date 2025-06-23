b6745ab51ee182b1a67738a2bf3d55348035dfbe
IgirubElam <igirubuntuelam@gmail.com>
2025-06-23 10:43:57 +0200

    second commit about change to deploy to render

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const https = require("https");
 const app = require('./src/app');
 const fs = require("fs");
 
-const isHttps = process.env.HTTPS ? process.env.HTTPS : false;
+// const isHttps = process.env.HTTPS ? process.env.HTTPS : false;
 
 let server;
 
-if (isHttps) {
-  const credentials = {
-    key: fs.readFileSync("/var/www/html/api/https/privkey.pem"),
-    cert: fs.readFileSync("/var/www/html/api/https/fullchain.pem"),
-  };
+// if (isHttps) {
+//   const credentials = {
+//     key: fs.readFileSync("/var/www/html/api/https/privkey.pem"),
+//     cert: fs.readFileSync("/var/www/html/api/https/fullchain.pem"),
+//   };
 
-  server = https.createServer(credentials, app);
-} else {
-  server = http.createServer(app);
-}
+//   server = https.createServer(credentials, app);
+// } else {
+//   server = http.createServer(app);
+// }
 
 server.listen(process.env.PORT || 4000);
 
